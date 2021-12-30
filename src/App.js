05f7d31e6eb3ef3efbb5bb99e4d06412fbb82152
corneliajohnson <corneliaj91@gmail.com>
2021-12-30 12:08:54 -0600

    change show/hide button

--- src/App.js
+++ src/App.js
@@ -55,7 +55,10 @@ function App() {
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTasks(!showAddTask)} />
+      <Header
+        onAdd={() => setShowAddTasks(!showAddTask)}
+        showAdd={showAddTask}
+      />
       {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
